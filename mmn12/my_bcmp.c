d77ad5a0873cfaf41b6ef5b2857a234224abcb97
Ido Sharon <michdodim@gmail.com>
2022-12-03 10:47:46 +0200

    define byte

--- mmn12/my_bcmp.c
+++ mmn12/my_bcmp.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
 #include "my_bcmp.h"
 
 int main (int argc, char *argv[]) {
-    char *s1 = "Hello";
-    char *s2 = "Hello";
-    char *s3 = "World";
-    printf("s1 == s2: %d\n", my_bcmp(s1, s2, 5));
-    printf("s1 == s3: %d\n", my_bcmp(s1, s3, 5));
+    char *str = "Hello, world! Hello, nice.";
+    char* p1 = &str[0];
+    char* p2 = &str[14];
+    // TODO: check if input is valid
+
+    printf("%s\n", (my_bcmp(p1, p2, 5) ? "Not same" : "same strings"));
+    
     return 0;
 }
 
 int my_bcmp (const void *b1, const void *b2, int len) {
-    unsigned char *p1 = b1;
-    unsigned char *p2 = b2;
-    while (len-- > 0) {
-        printf("p1: %hhu, p2: %hhu\n", *p1, *p2);
-        if (*p1++ != *p2++) {
-            return 1;
-        }
+    byte *p1 = (byte*)b1;
+    byte *p2 = (byte*)b2;
+    while(len-- > 0) {
+        if (*(p1++) != *(p2++)) return 1;
     }
     return 0;
 }
--- mmn12/my_bcmp.h
+++ mmn12/my_bcmp.h
@@ -1,2 +1,4 @@
+#include <stdio.h>
 
 int my_bcmp (const void *b1, const void *b2, int len);
+typedef unsigned char byte;
